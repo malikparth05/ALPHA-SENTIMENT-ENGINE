57499381ae0cb13361aaa1a1603e276708dd9303
malikparth05 <mparth6003@gmail.com>
2026-02-26 01:55:01 +0530

    Fix IndentationError

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ def api_company(ticker):
 
 if __name__ == "__main__":
     import os
-        port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", debug=False, port=port)
